780db54e35179b9a4d3592e46bebab32f50e4d9c
Matteo Caberlotto <mcaber@gmail.com>
2016-11-06 11:53:16 +0100

    redirect routes admin

---
+++ Admin/RouteAdmin.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Eight\PageBundle\Admin;
+
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
+
+
+class RouteAdmin extends AbstractAdmin
+{
+    protected $container;
+
+    public function createQuery($context = 'list')
+    {
+        $query = parent::createQuery($context);
+
+        $query
+            ->andWhere($query->getRootAlias() . '.controller LIKE :redirect_controller')
+            ->setParameter('redirect_controller', 'RaindropRoutingBundle:Generic:redirectRoute');
+
+        return $query;
+    }
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    // Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('path')
+            ->add('content', 'sonata_type_model', array(
+                // 'btn_add' => true,
+                // 'mapped' => false,
+                'class' => 'Raindrop\RoutingBundle\Entity\Route',
+                ))
+        ;
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('path')
+        ;
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('path')
+            ->add('routeContent', null, array(
+                'label' => 'Redirects to...',
+                'template' => 'EightPageBundle:Admin:_route_content.html.twig',
+                ))
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'edit' => array(),
+                )))
+        ;
+    }
+
+    public function preUpdate($object)
+    {
+        $this->setupRoute($object);
+    }
+
+    public function prePersist($object)
+    {
+        $this->setupRoute($object);
+    }
+
+    protected function setupRoute($route)
+    {
+        if (empty($route->getName())) {
+            $route->setNameFromPath();
+        }
+
+        if (empty($route->getController())) {
+            $route->setController('RaindropRoutingBundle:Generic:redirectRoute');
+        }
+    }
+}
---
+++ Controller/RouteCRUDController.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace Eight\PageBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Sonata\AdminBundle\Controller\CRUDController;
+
+class RouteCRUDController extends CRUDController
+{
+    /**
+     * Create action.
+     *
+     * @param Request $request
+     *
+     * @return Response
+     *
+     * @throws AccessDeniedException If access is not granted
+     */
+    public function createAction()
+    {
+        $request = $this->getRequest();
+        // the key used to lookup the template
+        $templateKey = 'edit';
+
+        $this->admin->checkAccess('create');
+
+        $class = new \ReflectionClass($this->admin->hasActiveSubClass() ? $this->admin->getActiveSubClass() : $this->admin->getClass());
+
+        if ($class->isAbstract()) {
+            return $this->render(
+                'SonataAdminBundle:CRUD:select_subclass.html.twig',
+                array(
+                    'base_template' => $this->getBaseTemplate(),
+                    'admin' => $this->admin,
+                    'action' => 'create',
+                ),
+                null,
+                $request
+            );
+        }
+
+        $object = $this->admin->getNewInstance();
+
+        /**
+         * It is required to bind the content resolver, else the binding will silently fail (form fw bug?).
+         */
+        $object->setResolver($this->container->get('raindrop_routing.content_resolver'));
+
+        $preResponse = $this->preCreate($request, $object);
+        if ($preResponse !== null) {
+            return $preResponse;
+        }
+
+        $this->admin->setSubject($object);
+
+        /** @var $form \Symfony\Component\Form\Form */
+        $form = $this->admin->getForm();
+        $form->setData($object);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            //TODO: remove this check for 4.0
+            if (method_exists($this->admin, 'preValidate')) {
+                $this->admin->preValidate($object);
+            }
+            $isFormValid = $form->isValid();
+
+            // persist if the form was valid and if in preview mode the preview was approved
+            if ($isFormValid && (!$this->isInPreviewMode($request) || $this->isPreviewApproved($request))) {
+                $this->admin->checkAccess('create', $object);
+
+                try {
+                    $object = $this->admin->create($object);
+
+                    if ($this->isXmlHttpRequest()) {
+                        return $this->renderJson(array(
+                            'result' => 'ok',
+                            'objectId' => $this->admin->getNormalizedIdentifier($object),
+                        ), 200, array());
+                    }
+
+                    $this->addFlash(
+                        'sonata_flash_success',
+                        $this->trans(
+                            'flash_create_success',
+                            array('%name%' => $this->escapeHtml($this->admin->toString($object))),
+                            'SonataAdminBundle'
+                        )
+                    );
+
+                    // redirect to edit mode
+                    return $this->redirectTo($object);
+                } catch (ModelManagerException $e) {
+                    $this->handleModelManagerException($e);
+
+                    $isFormValid = false;
+                }
+            }
+
+            // show an error message if the form failed validation
+            if (!$isFormValid) {
+                if (!$this->isXmlHttpRequest()) {
+                    $this->addFlash(
+                        'sonata_flash_error',
+                        $this->trans(
+                            'flash_create_error',
+                            array('%name%' => $this->escapeHtml($this->admin->toString($object))),
+                            'SonataAdminBundle'
+                        )
+                    );
+                }
+            } elseif ($this->isPreviewRequested()) {
+                // pick the preview template if the form was valid and preview was requested
+                $templateKey = 'preview';
+                $this->admin->getShow();
+            }
+        }
+
+        $view = $form->createView();
+
+        // set the theme for the current Admin Form
+        $this->get('twig')->getExtension('form')->renderer->setTheme($view, $this->admin->getFormTheme());
+
+        return $this->render($this->admin->getTemplate($templateKey), array(
+            'action' => 'create',
+            'form' => $view,
+            'object' => $object,
+        ), null);
+    }
+}
