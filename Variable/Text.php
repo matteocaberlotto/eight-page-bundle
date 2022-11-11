f29c8ad4daeff32595f3ae333f18959f3df8e026
Matteo Caberlotto <mcaber@gmail.com>
2022-11-11 16:22:29 +0100

    Fix: text variable (raw)

--- Variable/Text.php
+++ Variable/Text.php
@@ -7,24 +7,27 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Eight\PageBundle\Model\ContentInterface;
 use Eight\PageBundle\Variable\AbstractVariable;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class Text extends AbstractVariable
 {
     public function buildForm(FormBuilderInterface $builder, $name, $config, ContentInterface $variable = null)
     {
         $editor_class_append = ' eight-page-textarea';
+        $class = HiddenType::class;
 
         // prevent rich text editor when requested
         if ($config->has('raw') && $config->get('raw')) {
             $editor_class_append = '';
+            $class = TextareaType::class;
         }
 
         $builder
-            ->add($name, HiddenType::class, [
+            ->add($name, $class, [
                 'label' => $config->has('label') ? $config->get('label') : $name,
                 'required' => false,
                 'attr' => [
-                    'class' => 'eight-textarea' . $editor_class_append,
+                    'class' => 'eight-textarea form-control ' . $editor_class_append,
                 ]])
             ;
     }
