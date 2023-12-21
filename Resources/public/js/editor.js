20827f9111b0660a98389c46f927bfb6aa2e0598
Matteo Caberlotto <mcaber@gmail.com>
2023-12-21 16:35:30 +0100

    Patch: quilljs setup

--- Resources/public/js/editor.js
+++ Resources/public/js/editor.js
@@ -93,6 +93,11 @@ var Editor = (function () {
                                 [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
                                 [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
 
+                                [
+                                    { 'color': window.quilljs.colors ?? [] },
+                                    { 'background': window.quilljs.backgrounds ?? [] }
+                                ],// dropdown with custom|defaults from theme
+
                                 [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
                                 [{ 'font': [] }],
                                 [{ 'align': [] }],
